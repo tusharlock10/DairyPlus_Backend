74a626af2b1f3c798dd6c56fdbe2d170792a8341
Tushar Jain <tusharlock10@gmail.com>
2019-11-07 18:20:04 +0530

    Fix

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express();
 const http = require('http').Server(app);
 const mongoose = require('mongoose');
 
+console.log("here before /")
 app.get("/", (req, res) => {
+  console.log("In get")
   res.send({hello:true});
 })
 
 
 const PORT = process.env.PORT || 8000
-http.listen(PORT)
+http.listen(PORT,()=>{
+  console.log("listening")
+})
