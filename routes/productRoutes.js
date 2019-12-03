318f98f6e9b02afd05f675565f3a44cc6d005b9c
Tushar Jain <tusharlock10@gmail.com>
2019-12-03 23:19:42 +0530

    Added admin panel and settings

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const Product = mongoose.model('Product');
+const Order = mongoose.model('Order');
 const sh = require('shorthash');
+
+ADMIN_PHONE = ["9354527144"]
 
 module.exports = (app) => {
 
@@ -18,6 +21,9 @@ module.exports = (app) => {
 
   app.get('/api/settings/', (req, res)=>{
     User.findById(req.headers.authorization).lean().select('name phone email address').then((user)=>{
+      if (ADMIN_PHONE.includes(user.phone)){
+        user['isAdmin'] = true
+      }
       res.send(user);
     });
   });
@@ -31,6 +37,38 @@ module.exports = (app) => {
       user.save();
       res.send(user);
     });
+  })
+
+  app.get('/api/admin/', (req, res)=>{
+    User.findById(req.headers.authorization).lean().select('phone').then((user)=>{
+      if (!ADMIN_PHONE.includes(user.phone)){
+        res.send("You are not authorized to do so")
+      }
+      else{
+        Order.find({is_delivered: false}).lean().then((orders)=>{
+          res.send(orders)
+        })
+      }
+    })
+  })
+
+  app.post('/api/admin/delivered/', (req, res) => {
+    User.findById(req.headers.authorization).lean().select('phone').then((user)=>{
+      if (!ADMIN_PHONE.includes(user.phone)){
+        res.send("You are not authorized to do so")
+      }
+      else{
+        Order.findById(req.body.order_id).then((order) => {
+          order.is_delivered = true;
+          order.date_of_delivery = Date.now();
+          order.status = "delivered";
+          order.isPaid = true;
+
+          order.save().then(()=>{res.send("Order is now delivered");})
+
+        })
+      }
+    })
   })
 
 }
